be0ffbf6cf6754139f90f656e7c37364c6e466fc
Stephanie Qie <44703853+sqie@users.noreply.github.com>
2020-05-15 17:11:55 -0400

    created blueprint ui

---
+++ MyRecipes/MyRecipes/RecipeController.swift
@@ -0,0 +1,30 @@
+//
+//  RecipeController.swift
+//  MyRecipes
+//
+//  Created by Stephanie Qie on 5/15/20.
+//  Copyright © 2020 Stephanie Qie. All rights reserved.
+//
+
+import UIKit
+
+class RecipeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyRecipes/MyRecipes/RecipesController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyRecipes
+//
+//  Created by Stephanie Qie on 5/15/20.
+//  Copyright © 2020 Stephanie Qie. All rights reserved.
+//
+
+import UIKit
+
+class RecipesController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
